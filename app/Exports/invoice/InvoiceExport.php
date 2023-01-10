552c19aa70308d364e94cc62697a0e0b3de9c429
Manuel Gomez <jmgc@virket.com>
2023-01-09 22:48:31 -0600

    export: start row on excel, diferent format files, filters

--- app/Exports/invoice/InvoiceExport.php
+++ app/Exports/invoice/InvoiceExport.php
@@ -3,17 +3,32 @@
 namespace App\Exports\invoice;
 
 use App\Models\Invoice;
+use Maatwebsite\Excel\Excel;
+use Maatwebsite\Excel\Concerns\Exportable;
+use Illuminate\Contracts\Support\Responsable;
 use Maatwebsite\Excel\Concerns\FromCollection;
 use Maatwebsite\Excel\Concerns\WithCustomStartCell;
 
-class InvoiceExport implements FromCollection, WithCustomStartCell
+class InvoiceExport implements FromCollection, WithCustomStartCell, Responsable
 {
+    use Exportable;
+    // private $filename = 'invoices.csv';
+    // private $writerType = Excel::CSV;
+    private $fileName = 'invoices.xlsx';
+    private $writerType = Excel::XLSX;
+    private $filters;
+
+    public function __construct($filters){
+        $this->filters = $filters;
+    }
+
     /**
     * @return \Illuminate\Support\Collection
     */
     public function collection()
     {
-        return Invoice::all();
+        // return Invoice::all();
+        return Invoice::filters($this->filters)->get();
     }
 
     public function startCell(): string{
--- app/Http/Livewire/FilterInvoices.php
+++ app/Http/Livewire/FilterInvoices.php
@@ -21,7 +21,11 @@ class FilterInvoices extends Component
     ];
 
     public function generateReport(){
-        return Excel::download(new InvoiceExport(),'invoices.xlsx');
+        // return Excel::download(new InvoiceExport(),'invoices.xlsx');
+        // return Excel::download(new InvoiceExport(),'invoices.csv', \Maatwebsite\Excel\Excel::CSV);
+        // return (new InvoiceExport())->download(); //sin la implementacion Responsable
+        // return new InvoiceExport(); //con la implementacion Responsable
+        return new InvoiceExport($this->filters); //como es un metodo se pasan los filtros al constructor
     }
 
     public function render()
